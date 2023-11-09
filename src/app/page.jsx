a07ad9d605d229dac652ca970c0166f473754177
SimranArora1203 <113377852+SimranArora1203@users.noreply.github.com>
2023-11-09 19:56:19 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,15 +6,15 @@ import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
-  // return (
-  //   <div className={styles.container}>
-  //     <Featured />
-  //     <CategoryList />
-  //     <div className={styles.content}>
-  //       <CardList page={page} />
-  //       <Menu />
-  //     </div>
-  //   </div>
-  // );
-  return <div>hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList page={page} />
+        <Menu />
+      </div>
+    </div>
+  );
+  
 }
